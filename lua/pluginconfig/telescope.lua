f88ea9a9011524afa4f85a596e71452ef7daa3d2
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2023-03-31 17:17:30 +0200

    override missing symbols in telescope find_files

--- lua/pluginconfig/telescope.lua
+++ lua/pluginconfig/telescope.lua
@@ -5,3 +5,28 @@ require("telescope").setup({
 })
 
 require("telescope").load_extension("command_center")
+
+
+
+require("nvim-web-devicons").setup {
+ override = {
+  cpp = {
+    icon = "C",
+    color = "#428850",
+    cterm_color = "65",
+    name = "cpp"
+  },
+  h = {
+    icon = "H",
+    color = "#428850",
+    cterm_color = "65",
+    name = "h"
+  },
+  inl = {
+    icon = "I",
+    color = "#428850",
+    cterm_color = "65",
+    name = "inl"
+  }
+ };
+}
