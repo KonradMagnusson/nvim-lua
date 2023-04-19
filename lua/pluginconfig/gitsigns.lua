07f3e668b000c0df4a065996ca7557282828e0ad
Konrad Magnusson <konradma@kth.se>
2023-04-19 17:38:59 +0200

    add a gitsigns blame entry to command center

--- lua/pluginconfig/gitsigns.lua
+++ lua/pluginconfig/gitsigns.lua
@@ -16,14 +16,14 @@ require('gitsigns').setup {
     follow_files = true
   },
   attach_to_untracked = true,
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    delay = 0,
     ignore_whitespace = true,
   },
-  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
