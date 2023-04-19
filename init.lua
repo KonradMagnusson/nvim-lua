e867c217ef42c331ebe7b71b0d7a1e561a494be2
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2023-04-19 08:58:16 +0200

    nuke some profanity and dead code

--- init.lua
+++ init.lua
@@ -15,16 +15,6 @@ else
 	require("os.linux")
 end
 
-if vim.api.nvim_win_get_option(0, "diff") then
-	vim.notify("Yer in diffmode, cunt!", vim.log.levels.INFO)
-	vim.api.nvim_set_keymap( "n", "<leader>n", "[c", { noremap = true } )
-	vim.api.nvim_set_keymap( "n", "<leader>p", "]c", { noremap = true } )
-
-	vim.api.nvim_set_keymap( "n", "<leader>1", "<CMD>diffget LOCAL<CR>", { noremap = true } )
-	vim.api.nvim_set_keymap( "n", "<leader>2", "<CMD>diffget BASE<CR>", { noremap = true } )
-	vim.api.nvim_set_keymap( "n", "<leader>3", "<CMD>diffget REMOTE<CR>", { noremap = true } )
-end
-
 function ReloadConfig()
 	for k, _ in pairs(package.loaded) do
 		if _G.__CleanSetup[k] == nil then
