916aa448df7f495ce876c8df9d999df7fe4ae59a
Konrad Magnusson <konradma@kth.se>
2023-04-19 17:31:27 +0200

    extend colorset in va

--- lua/colors/va.lua
+++ lua/colors/va.lua
@@ -2,35 +2,48 @@ local lush = require('lush')
 local hsl = lush.hsl
 
 -- Colors
-local black 		= hsl("#000000")
-local charcoal		= hsl("#151515")
-local graphite 		= hsl("#2c2c2c")
-local lead			= hsl("#332c50")
-local gunmetal 		= hsl("#8d918d")
-local slate 		= hsl("#c8c8c8")
-local white 		= hsl("#f0f0f0")
+local black					= hsl("#000000")
+local charcoal				= hsl("#151515")
+local graphite 				= hsl("#2c2c2c")
+local lead					= hsl("#332c50")
+local heavy_cloud			= hsl("#706b66")
+local gunmetal 				= hsl("#8d918d")
+local dust					= hsl("#a89f94")
+local slate 				= hsl("#c8c8c8")
+local white 				= hsl("#f0f0f0")
 
-local virus 		= hsl("#a32da6")
-local pinkface		= hsl("#FF00FF")
-local lavender 		= hsl("#d8a0df")
-local sky_blue 		= hsl("#0078db")
-local light_blue 	= hsl("#569cd6")
-local jade			= hsl("#009393")
-local cote_dazur 	= hsl("#00CCFF")
-local cloud 		= hsl("#add8e6")
-local mint			= hsl("#90ee90")
-local venom 		= hsl("#00ff00")
-local leaf			= hsl("#007700")
-local olive			= hsl("#95ae88")
-local light_olive 	= hsl("#b5cea8")
-local beige 		= hsl("#e9d798")
-local light_mocha 	= hsl("#d69d85")
-local mocha 		= hsl("#ab654b")
-local mahogany 		= hsl("#954a4a")
-local crimson		= hsl("#dc143c")
-local clownfish 	= hsl("#ff8000")
-local pumpkin		= hsl("#d75f00")
-local dandelion 	= hsl("#ffff00")
+local virus 				= hsl("#a32da6")
+local pinkface				= hsl("#FF00FF")
+local lavender 				= hsl("#d8a0df")
+local matte_sapphire		= hsl("#5f87d7")
+local morning_sky			= hsl("#2d93dd")
+local light_blue 			= hsl("#569cd6")
+local sky_blue 				= hsl("#0078db")
+local sage					= hsl("#5a857a")
+local blue_clay				= hsl("#46878f")
+local jade					= hsl("#009393")
+local turqoise				= hsl("#20b0c0")
+local cote_dazur 			= hsl("#00CCFF")
+local cloud 				= hsl("#add8e6")
+local mint					= hsl("#90ee90")
+local kiwi					= hsl("#94e344")
+local venom 				= hsl("#00ff00")
+local cactus				= hsl("#426e5d")
+local leaf					= hsl("#007700")
+local faded_green			= hsl("#77af68")
+local moss					= hsl("#6b8c42")
+local olive					= hsl("#95ae88")
+local light_olive 			= hsl("#b5cea8")
+local beige 				= hsl("#e9d798")
+local light_mocha 			= hsl("#d69d85")
+local mocha 				= hsl("#ab654b")
+local mahogany 				= hsl("#954a4a")
+local crimson				= hsl("#dc143c")
+local rosso_corsa			= hsl("#D70000")
+local pumpkin				= hsl("#d75f00")
+local papaya				= hsl("#d78700")
+local clownfish 			= hsl("#ff8000")
+local dandelion 			= hsl("#ffff00")
 
 
 
@@ -116,6 +129,17 @@ local qnrd_theme = lush(function(injected_functions)
 		DiffDelete						{ bg = crimson.darken(15) },
 		DiffChange						{ bg = pumpkin.darken(15),									gui = 'BOLD,ITALIC'},
 		DiffText						{								fg = pumpkin,				gui = 'BOLD,ITALIC'},
+
+		GitSignsAdd						{								fg = venom },
+		GitSignsAddNr					{								fg = venom },
+		GitSignsChange					{								fg = clownfish },
+		GitSignsChangeNr				{								fg = clownfish },
+		GitSignsDelete					{ 								fg = rosso_corsa },
+		GitSignsDeleteNr				{ 								fg = rosso_corsa },
+		GitSignsChangeddelete			{ 								fg = rosso_corsa,			gui = "ITALIC" },
+		GitSignsChangeddeleteNr			{ 								fg = rosso_corsa,			gui = "ITALIC" },
+		GitSignsCurrentLineBlame		{								fg = gunmetal },
+
 
 		Directory						{								fg = jade },
 
