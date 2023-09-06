0d83f5b85b139f764d778fb2d41328c32522b4c9
Konrad Magnusson <konradma@kth.se>
2023-09-07 00:38:52 +0200

    compacy notifications

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,5 +3,28 @@ return {
 
 	dependencies = { "rktjmp/lush.nvim" },
 
-	config = function() vim.notify = require("notify") end
+	config = function()
+		vim.notify = require("notify")
+		vim.notify.setup({
+			max_width = 200,
+			max_height = 200,
+			on_open = function() end,
+			on_close = function() end,
+			background_colour = "NotifyBackground",
+			fps = 30,
+			icons = {
+			  DEBUG = "",
+			  ERROR = "",
+			  INFO = "",
+			  TRACE = "✎",
+			  WARN = ""
+			},
+			level = 1,
+			minimum_width = 50,
+			render = "compact",
+			stages = "fade_in_slide_out",
+			timeout = 5000,
+			top_down = true
+		  })
+	end
 }
