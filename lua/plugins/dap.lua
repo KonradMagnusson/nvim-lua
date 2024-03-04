22f184bd216eb14d807b82c0f1f45cbf91b0c778
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2024-03-04 10:19:01 +0100

    QoL improvements to dap setup - cmdline args & better selection functionality

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,8 +2,8 @@ local set_map = function( keys, action )
 	vim.api.nvim_set_keymap("n", keys, action, { noremap = true } )
 end
 
-set_map( "<leader>db", "<CMD>lua require('dap').toggle_breakpoint()<CR>" )
-set_map( "<leader>dB", "<CMD>lua require('dap').toggle_breakpoint(vim.fn.input('Condition: '))<CR>" )
+set_map( "<leader>db", "<CMD>lua require('persistent-breakpoints.api').toggle_breakpoint()<CR>" )
+set_map( "<leader>dB", "<CMD>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<CR>" )
 set_map( "<leader>dlp", "<CMD>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>" )
 
 set_map( "<leader>dc", "<CMD>lua require('dap').continue()<CR>" )
@@ -14,14 +14,53 @@ set_map( "<leader>di", "<CMD>lua require('dap').step_into()<CR>" )
 set_map( "<leader>do", "<CMD>lua require('dap').step_out()<CR>" )
 
 
--- according to DAP, callstacks grow downwards
+-- According to DAP, stacks grow downwards. This is somewhat sensible, considering they often do in terms of memory addressing, but very few people
+-- would say you "pop something off the bottom of the stack".
 set_map( "<C-A-down>", "<CMD>lua require('dap').up()<CR>" )
 set_map( "<C-A-up>", "<CMD>lua require('dap').down()<CR>" )
 
 
-local init_dap = function()
+-- these aren't blocking per se - they return a coroutine that dap configurations are forced to wait for
+function BlockingUIInput(  opts )
+	return coroutine.create( function( co ) vim.ui.input( opts, function( txt ) coroutine.resume( co, txt ) end ) end )
+end
+
+function BlockingUISelect( items, opts )
+	return coroutine.create( function( co ) vim.ui.select( items, opts, function( selection ) coroutine.resume( co, selection ) end ) end )
+end
+
+
+function GetAvailableVickyBinaries()
+	return coroutine.create(function( co )
+		local bin_dir = vim.fn.getcwd() .. "/build/binaries/"
+		local bins = {}
+		for bin in io.popen("ls " .. bin_dir .. "victoria3*"):lines() do
+			bin = bin:gsub( bin_dir, "" )
+			table.insert( bins, bin )
+		end
+		vim.ui.select( bins, { label = "bin: " }, function( bin ) coroutine.resume( co, bin_dir .. bin ) end )
+	end)
+end
+
+function GetOrSetVickyArgs()
+	return coroutine.create(
+		function( co )
+			vim.ui.input(
+			{ prompt = "Args: ", default = vim.g.VICKY_ARGS },
+			function( selection )
+				vim.g.VICKY_ARGS = selection
+				coroutine.resume( co, selection )
+			end )
+		end )
+end
+
+local init_dap = function( opts )
 	local dap = require("dap")
 	local daputils = require("dap.utils")
+
+	if vim.g.VICKY_ARGS == nil or type(vim.g.VICKY_ARGS) ~= "string" then
+		vim.g.VICKY_ARGS = ""
+	end
 
 	dap.defaults.fallback.external_terminal = {
 		command = "/usr/bin/alacritty",
@@ -29,8 +68,8 @@ local init_dap = function()
 	}
 
 	dap.adapters.lldb = {
-		type = 'executable',
-		command = '/usr/bin/lldb-vscode',
+		type = "executable",
+		command = "/usr/bin/lldb-vscode",
 		name = "lldb",
 		options = {
 			initialize_timeout_sec = 10,
@@ -39,39 +78,32 @@ local init_dap = function()
 
 	dap.configurations.cpp = {
 		{
-			name = "Launch",
+			name = "Debug V3",
 			type = "lldb",
 			request = "launch",
-			program = function()
-				return coroutine.create(function(dap_run_co)
-					local bin_dir = vim.fn.getcwd() .. "/build/binaries/"
-					local bins = {}
-					for bin in io.popen("ls " .. bin_dir .. "victoria3*"):lines() do
-						bin = bin:gsub( bin_dir, "" )
-						table.insert( bins, bin )
-					end
-					vim.ui.select( bins, { label = "bin: " }, function( bin ) coroutine.resume(dap_run_co, bin_dir .. bin ) end )
-				end)
-			end,
-			cwd = '${workspaceFolder}',
+			program = GetAvailableVickyBinaries,
+			args = GetOrSetVickyArgs,
+			cwd = "${workspaceFolder}",
 			stopOnEntry = false,
 			runInTerminal = false,
-			--args = { '-skip' },
-			--args = { '-skip', '-handsoff' },
-			--args = { '-skip', '-handsoff'},-- '-nographics' },
-			args = { '-loadsave=nerd' },
-			--args = { '-loadsave=ctd' }, --, '-handsoff', '-nographics' },
-			--args = { '-handsoff'},
-			--args = { '-mapeditor' },
 		},
 		{
-			name = "Attach",
+			name = "Debug V3 [noargs]",
 			type = "lldb",
-			request = "attach",
-			cwd = '${workspaceFolder}',
+			request = "launch",
+			program = GetAvailableVickyBinaries,
+			cwd = "${workspaceFolder}",
 			stopOnEntry = false,
 			runInTerminal = false,
-			pid = function() return daputils.pick_process({ filter = 'victoria3' }) end
+		},
+		{
+			name = "Attach to running game",
+			type = "lldb",
+			request = "attach",
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+			runInTerminal = false,
+			pid = function() return daputils.pick_process({ filter = "victoria3" }) end
 		}
 	}
 end
@@ -79,6 +111,5 @@ end
 
 
 return {
-	"mfussenegger/nvim-dap",
-	init = init_dap
+  "mfussenegger/nvim-dap", init = init_dap
 }
