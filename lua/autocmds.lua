ac9f3402ec5d5c4747ab7defafc13fad1a21b161
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2024-11-30 22:39:00 +0100

    smol aucmd refactor

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,55 +1,57 @@
+local au = vim.api.nvim_create_autocmd
+
 local file_type_group = vim.api.nvim_create_augroup("fileTypeCommands", { clear = true })
 local buffer_group = vim.api.nvim_create_augroup("bufCommands", { clear = true })
 
 -- toggle relativenumber / norelativenumber based on mode
-vim.api.nvim_create_autocmd("InsertEnter", {
+au("InsertEnter", {
 	command = "set norelativenumber",
 })
-vim.api.nvim_create_autocmd("InsertLeave", {
+au("InsertLeave", {
 	command = "set relativenumber",
 })
 
 -- makefiles require tabs
-vim.api.nvim_create_autocmd("fileType", {
+au("fileType", {
 	group = file_type_group,
 	pattern = "make",
 	command = "setlocal ts=4 sts=4 sw=4 noexpandtab",
 })
 
 -- vertical help
-vim.api.nvim_create_autocmd("fileType", {
+au("fileType", {
 	group = file_type_group,
 	pattern = "help",
 	command = "wincmd L",
 })
 
 -- yaml requires spaces
-vim.api.nvim_create_autocmd("fileType", {
+au("fileType", {
 	group = file_type_group,
 	pattern = "yaml",
 	command = "setlocal ts=2 sts=2 sw=2 expandtab",
 })
 
--- pretend cw gui files are regular script files 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+-- pretend cw gui files are regular script files
+au({"BufNewFile", "BufRead"}, {
 	group = file_type_group,
 	pattern = "*.gui",
 	command = "setf text",
 })
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+au({"BufNewFile", "BufRead"}, {
 	group = file_type_group,
 	pattern = "*.tok",
 	command = "setf tok",
 })
 
 -- (local) clang-format on write
-vim.api.nvim_create_autocmd("BufWritePost", {
+au("BufWritePost", {
 	pattern = {"*.cpp", "*.h", "*.inl" },
 	command = "silent !/home/qnrd/bin/clang-format -i %:p"
 })
 
 -- enter insert mode when switching to a terminal window
-vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+au({ "BufWinEnter", "WinEnter" }, {
 	group = buffer_group,
 	pattern = "term://*",
 	command = "startinsert",
@@ -57,7 +59,7 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
 
 
 -- diff-specific mappings
-vim.api.nvim_create_autocmd( {"VimEnter"}, {
+au( {"VimEnter"}, {
 	callback = function(ev)
 		if vim.api.nvim_win_get_option(0, "diff") then
 			require("diff")
@@ -66,24 +68,27 @@ vim.api.nvim_create_autocmd( {"VimEnter"}, {
 })
 
 
-vim.api.nvim_create_autocmd( "fileType", {
+au( "fileType", {
 	pattern = "cpp",
 	callback = function(ev)
 		vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
 		vim.api.nvim_buf_set_option(0, "matchpairs", "(:),{:},[:],<:>" )
+		if vim.fn.findfile( "/tmp/.build_token" ) then
+			os.execute( "ln -sf " .. vim.v.servername .. " /tmp/.nvim_sock" )
+		end
 	end
 })
 
 -- hide inlay hints in insert mode
-vim.api.nvim_create_autocmd("InsertEnter", {
+au("InsertEnter", {
 	command = "ClangdDisableInlayHints",
 })
-vim.api.nvim_create_autocmd("InsertLeave", {
+au("InsertLeave", {
 	command = "ClangdSetInlayHints",
 })
 
 -- dynamic scrolloff based on window height
-vim.api.nvim_create_autocmd( {"WinResized", "WinNew", "WinEnter"}, {
+au( {"WinResized", "WinNew", "WinEnter"}, {
 	callback = function(ev)
 		local window_height = vim.api.nvim_win_get_height( 0 )
 		local offset = math.floor( window_height / 6 )
