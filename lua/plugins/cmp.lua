594d95439c79c9394205e94834822e9ca7ad348e
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2025-02-14 08:44:47 +0100

    improve lsp completion sorting

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,12 +2,45 @@ return {
 	"hrsh7th/nvim-cmp",
 
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp"
+		"hrsh7th/cmp-nvim-lsp",
+		"lukas-reineke/cmp-under-comparator",
+		"ofirgall/cmp-lspkind-priority"
 	},
 
 	init = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local lspkind_priority = require('cmp-lspkind-priority')
+		lspkind_priority.setup {
+			-- Default priority by nvim-cmp
+			priority = {
+				'Snippet',
+				'Variable',
+				'Method',
+				'Function',
+				'Constructor',
+				'Property',
+				'Field',
+				'Class',
+				'Interface',
+				'Module',
+				'Unit',
+				'Value',
+				'Enum',
+				'Keyword',
+				'Color',
+				'File',
+				'Reference',
+				'Folder',
+				'EnumMember',
+				'Constant',
+				'Struct',
+				'Event',
+				'Operator',
+				'TypeParameter',
+				'Text',
+			}
+		}
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -45,6 +78,17 @@ return {
 				{ name = "nvim_lsp" },
 				{ name = "luasnip", option = { show_autosnippets = true } },
 			}),
+			sorting = {
+				comparators = {
+					lspkind_priority.compare,
+					cmp.config.compare.locality,
+					cmp.config.compare.exact,
+					cmp.config.compare.offset,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					require("cmp-under-comparator").under,
+				}
+			},
 		})
 	end
 }
