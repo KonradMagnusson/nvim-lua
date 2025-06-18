b9935bf58bef6f3018dc90f0c6affa72d0ec222d
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2025-06-18 14:20:39 +0200

    reduce DAP noise

--- lua/plugins/dap-virtual-text.lua
+++ lua/plugins/dap-virtual-text.lua
@@ -1,7 +1,7 @@
 return {
 	"theHamsta/nvim-dap-virtual-text",
 	opts = {
-		enabled = true,
+		enabled = false,
 		enabled_commands = true,
 		highlight_changed_variables = true,
 		highlight_new_as_changed = false,
